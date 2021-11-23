ae43603e70e45d8e70f99d978ecc294e979e0804
nidhijainpnp <12njain06@gmail.com>
2021-11-24 00:15:01 +0530

    updating gitignore

--- onlineEngagement/managerApp/models.py
+++ onlineEngagement/managerApp/models.py
@@ -5,7 +5,7 @@ from django.db.models.fields import CharField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
  
-# #from phonenumber_field.modelfields import PhoneNumberField
+
 # # Create your models here.
 
 sexChoice = (
--- onlineEngagement/onlineEngagement/settings.py
+++ onlineEngagement/onlineEngagement/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'managerApp.apps.ManagerappConfig',
-    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
